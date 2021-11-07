526b49e7c6a655a4d36bd7f085ddb97b1529f8dd
Chiara <dulcechiara@gmail.com>
2021-11-07 20:39:36 -0300

    index controller

--- springboot-backend/src/main/java/springboot/controller/IndexController.java
+++ springboot-backend/src/main/java/springboot/controller/IndexController.java
@@ -1,2 +1,12 @@
-package springboot.controller;public class IndexController {
+package springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @GetMapping("/")
+    public String index() {
+        return "Hello there! I'm running.";
+    }
 }
